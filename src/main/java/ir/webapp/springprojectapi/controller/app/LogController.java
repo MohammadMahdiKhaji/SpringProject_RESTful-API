a426e43ca8cd5d3d186181c030fc9a2fab7cc8ba
MohammadMahdi Khaji <mohammadmahdikhaji81@gmail.com>
2023-02-12 12:23:14 +0330

    Second Commit

--- src/main/java/ir/webapp/springprojectapi/controller/app/LogController.java
+++ src/main/java/ir/webapp/springprojectapi/controller/app/LogController.java
@@ -1,21 +1,21 @@
-//package ir.webapp.springprojectapi.controller.app;
-//
-//import ir.webapp.springprojectapi.model.entity.Log;
-//import ir.webapp.springprojectapi.model.service.LogService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//
-//@Controller
-//public class LogController {
-//
-//    private final LogService logService;
-//
-//    @Autowired
-//    public LogController(LogService logService){
-//        this.logService = logService;
-//    }
-//
-//    public Log save(Log log) {
-//            return logService.save(log);
-//    }
-//}
+package ir.webapp.springprojectapi.controller.app;
+
+import ir.webapp.springprojectapi.model.entity.Log;
+import ir.webapp.springprojectapi.model.service.LogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LogController {
+
+    private final LogService logService;
+
+    @Autowired
+    public LogController(LogService logService){
+        this.logService = logService;
+    }
+
+    public Log save(Log log) {
+            return logService.save(log);
+    }
+}
--- src/main/java/ir/webapp/springprojectapi/model/entity/base/BaseEntity.java
+++ src/main/java/ir/webapp/springprojectapi/model/entity/base/BaseEntity.java
@@ -1,12 +1,9 @@
 package ir.webapp.springprojectapi.model.entity.base;
 
-//import ir.webapp.springprojectapi.controller.app.LogController;
-//import ir.webapp.springprojectapi.controller.app.UserAccessController;
-//import ir.webapp.springprojectapi.model.entity.Log;
-//import ir.webapp.springprojectapi.model.entity.enumeration.ActionType;
-//import ir.webapp.springprojectapi.model.entity.enumeration.DbTable;
-//import ir.webapp.springprojectapi.model.repository.LogDA;
-//import ir.webapp.springprojectapi.model.service.LogService;
+import ir.webapp.springprojectapi.controller.app.LogController;
+import ir.webapp.springprojectapi.model.entity.Log;
+import ir.webapp.springprojectapi.model.entity.enumeration.ActionType;
+import ir.webapp.springprojectapi.model.entity.enumeration.DbTable;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -28,38 +25,26 @@ public class BaseEntity {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
-//    @Transient
-//    Logger logger = LoggerFactory.getLogger(BaseEntity.class);
-
 //    @Version
 //    private long versionId;
 
-//    //log
-//    @Transient
-//    private final LogController logController;
-//
-//    @Autowired
-//    public BaseEntity(LogController logController){
-//        this.logController = logController;
-//    }
-//
-//    @PostPersist
-//    private void saveLog() {
-//        logger.trace(new Log(
-//                DbTable.valueOf(this.getClass().getSimpleName()),
-//                1,
-//                ActionType.Persist,
-//                this.toString(),
-//                "IP").toString());
-//    }
-//
-//    @PostUpdate
-//    private void editLog() {
-//        logger.trace(new Log(
-//                DbTable.valueOf(this.getClass().getSimpleName()),
-//                1,
-//                ActionType.Update,
-//                this.toString(),
-//                "IP").toString());
-//    }
+    @PostPersist
+    private void saveLog() {
+        System.out.println(new Log(
+                DbTable.valueOf(this.getClass().getSimpleName()),
+                1,
+                ActionType.Persist,
+                this.toString(),
+                "IP").toString());
+    }
+
+    @PostUpdate
+    private void editLog() {
+        System.out.println(new Log(
+                DbTable.valueOf(this.getClass().getSimpleName()),
+                1,
+                ActionType.Update,
+                this.toString(),
+                "IP").toString());
+    }
 }
